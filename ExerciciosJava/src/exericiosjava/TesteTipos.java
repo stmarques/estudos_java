7d9bdf9dcdf404d0f76559c9ee4ffc8420741c25
Amanda <83442480+stmarques@users.noreply.github.com>
2023-03-29 22:30:43 -0300

    Add files via upload

---
+++ ExerciciosJava/src/exericiosjava/TesteTipos.java
@@ -0,0 +1,21 @@
+package exericiosjava;
+
+public class TesteTipos {
+	
+	public static void main(String[] args) {
+		
+		 /*int idade = 30;
+		 String valor = Integer.toString(idade);
+		 System.out.println(idade);*/
+		
+		/*String valor = "30";
+		int idade = Integer.parseInt(valor);
+		System.out.println(idade);*/
+		
+		String valor = "30.5";
+		float idade = Float.parseFloat(valor);
+		System.out.printf("%.3f",idade);
+		
+	}
+
+}
