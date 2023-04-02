7387b63b2c5d8a6cb6b29fee3200dd6ba7eaaf14
Amanda Marques <83442480+stmarques@users.noreply.github.com>
2023-04-01 23:16:54 -0300

    Add files via upload

---
+++ Comentarios.java
@@ -0,0 +1,24 @@
+package exericiosjava;
+
+public class Comentarios {
+
+	public static void main(String[] args) {
+		// Comentário de uma linha
+		
+		/*
+		 *Nos comentarios de muitas linhas, 
+		 *podemos escrever o quanto quiser
+		 *até indicarmos o final do comentário
+		 */
+		
+		/**
+		 Exemplo para o comentário de documentação
+		 @author Amanda Marques
+		 @version 1.0
+		 @since 2023	
+		 */
+		
+
+	}
+
+}
